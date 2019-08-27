d50777aac2b98df723b297e945bebb54afbee252
yassh <yassh32@outlook.jp>
2019-08-27 13:23:57 +0900

    Hello World server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const message: string = "Hello World"
+import * as http from "http"
 
-console.log(message)
+const PORT = 8080
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end("Hello World\n")
+  })
+  .listen(PORT, () => console.log(`http://localhost:${PORT}`))
