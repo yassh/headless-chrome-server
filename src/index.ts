4651c0a3a377b3d2bbaa060fff49b5cecbf88f81
yassh <yassh32@outlook.jp>
2019-08-27 20:01:48 +0900

    headless-chrome-server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import * as http from "http"
+import * as puppeteer from "puppeteer"
 
-const PORT = 8080
+const PORT = 8888
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" })
-    res.end("Hello World\n")
-  })
-  .listen(PORT, () => console.log(`http://localhost:${PORT}`))
+async function main() {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.emulate(puppeteer.devices["iPhone X"])
+
+  function open(url: string) {
+    console.log(`Open ${url}`)
+    page.goto(url)
+  }
+
+  http
+    .createServer((req, res) => {
+      const url = req.headers["x-url"]
+
+      if (url) {
+        const urlStr = Array.isArray(url) ? url[0] : url
+        open(urlStr)
+      }
+
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      res.setHeader("Access-Control-Allow-Headers", "*")
+      res.end()
+    })
+    .listen(PORT, () => console.log(`http://localhost:${PORT}`))
+}
+
+main()
